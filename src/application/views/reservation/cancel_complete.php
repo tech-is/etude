9b9105755147f3c057b8e28b3fb87a1e92e0ee55
Takeda Chiharu <takeda123pc@gmail.com>
2020-09-03 12:55:06 +0900

    #49 make_cancel_complete_view

---
+++ src/application/views/reservation/cancel_complete.php
@@ -0,0 +1,33 @@
+<body>
+  <div class="container-fruid">
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-info">
+            <div class="card-header">
+              <h1 class="card-title">予約キャンセル完了</h1>
+            </div>
+            <p>予約削除完了致しました。</br>またのご利用お待ちしております。</p>
+          </div><!--/.card-info -->
+        </div><!--/.card -->
+      </div><!--/.col -->
+    </div><!-- /.row -->
+  </div><!-- /.container-fluid -->
+  <script src="<?= base_url(); ?>fullcalendar-4.4.2/packages/core/locales-all.js"></script>
+  <!-- jQuery -->
+  <script src="<?= base_url(); ?>AdminLTE-3.0.5/plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="<?= base_url(); ?>AdminLTE-3.0.5/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- bs-custom-file-input -->
+  <script src="<?= base_url(); ?>AdminLTE-3.0.5/plugins/bs-custom-file-input/bs-custom-file-input.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="<?= base_url(); ?>AdminLTE-3.0.5/dist/js/adminlte.min.js"></script>
+  <!-- AdminLTE for demo purposes -->
+  <script src="<?= base_url(); ?>AdminLTE-3.0.5/dist/js/demo.js"></script>
+  <script type="text/javascript">
+  $(document).ready(function() {
+    bsCustomFileInput.init();
+  });
+  </script>
+</body>
+</html>
