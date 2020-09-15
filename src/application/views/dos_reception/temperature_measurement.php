22c2e69884faa500e5331e8ef9ae15d2a751083b
wakanawatanabe <wakanawatanabe@users.noreply.github.com>
2020-09-15 20:20:09 +0900

    #56 mod_temperature_measurement

--- src/application/views/dos_reception/temperature_measurement.php
+++ src/application/views/dos_reception/temperature_measurement.php
@@ -17,17 +17,17 @@
                 <li class="completed"><span class="bubble"></span>案内</li>
                 <li><span class="bubble"></span>全員終了</li>
               </ul>
-              <form role="form">
+              <form role="form" method="post" action="/Group/measurement">
                 <div class="card-body" >
                       <div class="form-group">
-                        <label for="exampleInputEmail1"><span class="label label-danger" style="color: red;">必須</span> 検温結果：</label>
-                        <input type="text" class="form-control" id="exampleInputEmail1" placeholder="36.8" required>
+                        <label for="exampleInputEmail1"><span class="label label-danger" style="color: red;">必須:</span> 検温結果を入力してください</label>
+                        <p><?=htmlspecialchars($_SESSION['full_name'][$visitor_num]) ??"ゲスト" ?>様</p>
+                        <input type="hidden" name="visitor_num" value="<?= $visitor_num ?>">
+                        <input type="number" step="0.1" max="42.0" min="34.5" class="form-control" id="exampleInputEmail1" placeholder="36.8" name="temperature" required>
                       </div>
-                    <!-- <p class="text-danger" style="text-align:center">まだ予約は確定されていません</p> -->
                 </div><!-- /.card-body -->
                 <div class="card-footer" style="text-align:center">
-                  <!-- <button type="submit" class="btn btn-secondary">戻る</button> -->
-                  <button type="submit" class="btn btn-primary">登録</button>
+                  <button type="submit" name="register" class="btn btn-primary">登録</button>
                 </div>
               </form>
           </div><!-- /.card-info -->
