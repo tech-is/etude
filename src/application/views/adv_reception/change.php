ac636c3966f22656eb24140eb4394d1370ed3089
mamimyojin <mamimyojin@users.noreply.github.com>
2020-10-03 13:48:51 +0900

    #74 modified_change_view

--- src/application/views/adv_reception/change.php
+++ src/application/views/adv_reception/change.php
@@ -18,60 +18,54 @@
                 <li class="completed"><span class="bubble"></span>案内</li>
                 <li><span class="bubble"></span>全員終了</li>
               </ul>
-              <form role="form">
+              <form role="form" method="post" action="/index.php/Group/change_confirmation">
                 <div class="card-body" >
                   <p class="text-danger" style="text-align:center"><small>予約時間の変更はできません</small></p>
                   <div class="row">
                   <ul class="col-5" style="text-align: right;">予約日時：</ul>
-                  <ul class="col-7">2020/06/30 10:00～15:00</ul>
+                  <ul class="col-7"><?php echo htmlspecialchars($booking_data['booking_date']); ?> <?php echo htmlspecialchars(date('G:i', strtotime($booking_data['start_time']))); ?>～<?php echo htmlspecialchars(date('G:i', strtotime($booking_data['end_time']))); ?></ul>
                 </div><!-- card-body -->
                 <p class="text-danger" style="text-align:center"><small>キャンセルされる方は、名前の横の✅の✔を外してください</small></p>
                 <p class="text-danger" style="text-align:center"><small>参加者氏名に変更がある場合は、✅した状態で入力し直してください</small></p>
                 <p class="text-danger" style="text-align:center"><small>この画面で参加者数の追加はできません</small></p>
 
                 <div class="cp_ipcheck" style="text-align:center">
-                  <label>
-                    <input type="checkbox" id="d_rb1" checked />
+                  <label class="col-12">
                     <div class="form-group">
+                      <p id="d_rb1" >
                       代表者氏名（参加者氏名１）
-                      <input type="text" class="form-control" id="exampleInputEmail1" placeholder="participant full name" required>
+                      <?php echo htmlspecialchars($booking_data['booker']); ?>
+                      </p>
                     </div>
                   </label>
-
-                  <label>
-                    <input type="checkbox" id="d_rb2" checked />
-                    <div class="form-group">
-                      参加者氏名２
-                      <input type="text" class="form-control" id="exampleInputEmail1" placeholder="participant full name" required>
-                    </div>
-                  </label>
-
-                  <label for="d_rb3" >
-                    <input type="checkbox" id="d_rb3" checked />
-                    <div class="form-group">
-                      参加者氏名３
-                      <input type="text" class="form-control" id="exampleInputEmail1" placeholder="participant full name" required>
-                    </div>
-                  </label>
-
-                  <label for="d_rb4" >
-                    <input type="checkbox" id="d_rb4" checked />
-                    <div class="form-group">
-                      参加者氏名４
-                      <input type="text" class="form-control" id="exampleInputEmail1" placeholder="participant full name" required>
-                    </div>
-                  </label>
-
-                  <label for="d_rb5" >
-                    <input type="checkbox" id="d_rb5" checked />
-                    <div class="form-group">
-                      参加者氏名５
-                      <input type="text" class="form-control" id="exampleInputEmail1" placeholder="participant full name" required>
-                    </div>
-                  </label>
+                    <?php if ($booking_data['people_num']!= 1) : ?>
+                      <?php if (!empty($visitor_data)) : ?>
+                        <?php for ($value=1; $value < $booking_data['people_num']; $value++) :?>
+                          <label class="col-12">
+                            <div class="form-group">
+                              <input type="checkbox"  name="check_flag[<?= $value; ?>]" id="rb<?=$value;?>" value=<?php echo htmlspecialchars($visitor_data[$value]['number'])?> checked />
+                              参加者氏名<?php echo htmlspecialchars($visitor_data[$value]['number']);?>&nbsp;&nbsp;
+                              <input id="name<?php echo htmlspecialchars($value)?>" type="text" name="name<?php echo htmlspecialchars($value);?>" value="<?php echo htmlspecialchars($visitor_data[$value]['name']);?>">
+                            </div>
+                          </label>
+                        <?php endfor; ?>
+                        <?php endif; ?>
+                      <?php endif; ?>
                 </div><!-- /.cp_ipcheck -->
                 <div class="card-footer" style="text-align:center">
-                  <button type="submit" class="btn btn-primary">確認画面へ</button>
+                  <button type="button" class="btn btn-secondary" onclick="location.href='/index.php/Group/validity_check'">戻る</a>
+                  <button type="submit" class="btn btn-primary" onclick="location.href='/index.php/Group/change_confirmation?id=<?php echo htmlspecialchars($booking_data['id']); ?>'">確認する</button>
+                  <input type="hidden" name="id" value="<?php echo htmlspecialchars($booking_data['id']); ?>">
+                  <!-- <input type="hidden" name="booker" value="<?php echo htmlspecialchars($booking_data['booker']); ?>"> -->
+                  <input type="hidden" name="people_num" value="<?php echo htmlspecialchars($booking_data['people_num']); ?>">
+                    <?php if ($booking_data['people_num']!= 1) : ?>
+                      <?php if (!empty($visitor_data)) : ?>
+                        <?php for ($value=1; $value < $booking_data['people_num']; $value++) :?>
+                          <input type="hidden" name="number[<?php echo htmlspecialchars($value);?>]" value="<?php echo htmlspecialchars($visitor_data[$value]['number']); ?>">
+                          <input type="hidden" name="name'<?php echo htmlspecialchars($value);?>'" >
+                        <?php endfor; ?>
+                      <?php endif; ?>
+                    <?php endif; ?>
                 </div>
               </form>
           </div><!--/.card-info -->
