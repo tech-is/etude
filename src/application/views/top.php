e6a50e074edf9258c29fa16b3cea7c8ce3435932
wakanawatanabe <wakanawatanabe@users.noreply.github.com>
2020-10-09 15:47:46 +0900

    #90 mod_top_view

--- src/application/views/top.php
+++ src/application/views/top.php
@@ -10,16 +10,17 @@
                 <div style="text-align:center; padding-top:30px; padding-bottom:5px;">
                   <h5>受付をする方法をお選びください</h5>
                 </div>
-              <form role="form">
+              <!-- <form role="form" method="post" action="/Group/dos_login"> -->
+              <form>
                 <div class="card-body">
                   <div class="col-lg-6" style="position: relative; display: flex; min-width:30%; min-height: 100px; margin: auto;">
                       <td>
-                        <button type="button" class="btn btn-block btn-outline-info btn-lg" style="min-height: 70px; margin: auto;">事前予約受付</button>
+                        <button type="button" name="btn_adv" onclick="location.href='<?= base_url(); ?>Group_adv/adv_login'" class="btn btn-block btn-outline-info btn-lg" style="min-height: 70px; margin: auto;">事前予約受付</button>
                       </td>
                   </div>
                   <div class="col-lg-6" style="position: relative; display: flex; min-width:30%; min-height: 100px; margin: auto;">
                     <td> 
-                      <button type="button" class="btn btn-block btn-outline-info btn-lg" style="min-height: 70px; margin: auto;">当日受付</button>
+                      <button type="button" name="btn_dos" onclick="location.href='<?= base_url(); ?>Group/dos_login'" class="btn btn-block btn-outline-info btn-lg" style="min-height: 70px; margin: auto;">当日受付</button>
                     </td>
                   </div>
                 </div><!-- /.card-body -->
